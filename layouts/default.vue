89b0b2b650edfa5e4ef240f4dcf14a924475d0f9
Sergio Azócar <sergioazoc@gmail.com>
2022-03-30 12:06:32 -0300

    add basic ui navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    layout
+    <div class="flex justify-between items-center p-3">
+      <img
+        class="w-20 h-20 rounded-full border-4 border-green-500"
+        src="@/public/assets/images/sergio-azocar.jpg"
+        alt="Sergio Azócar"
+      />
+
+      <div class="flex gap-3 text-emerald-400 font-bold">
+        <p>Blog</p>
+        <p>Sobre mi</p>
+        <p>Contacto</p>
+      </div>
+    </div>
     <slot />
   </div>
 </template>
