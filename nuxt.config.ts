491f38481f2d80a61bbef18cacdd32ef3354d6e9
Sergio Azócar <sergioazoc@gmail.com>
2022-04-13 00:58:35 -0400

    add fontawesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,8 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  buildModules: ['@nuxtjs/tailwindcss']
-
+  buildModules: ['@nuxtjs/tailwindcss'],
+  css: [
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ]
 })
---
+++ plugins/fontawesome.ts
@@ -0,0 +1,32 @@
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import {
+  faBars
+} from "@fortawesome/free-solid-svg-icons"
+
+// import {
+//   faChartBar
+// } from "@fortawesome/free-regular-svg-icons"
+
+import {
+  faInstagram,
+  faTwitch,
+  faTwitter,
+  faYoutube
+} from "@fortawesome/free-brands-svg-icons"
+
+library.add(
+  faBars,
+  faYoutube,
+  faTwitch,
+  faTwitter,
+  faInstagram,
+)
+
+// This is important, we are going to let Nuxt.js worry about the CSS
+config.autoAddCss = false
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.component('fa', FontAwesomeIcon)
+})
