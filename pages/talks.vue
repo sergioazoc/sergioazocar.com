0dc07761eb7cb759cac05267b10cb2d252d49476
Sergio Azócar <sergioazoc@gmail.com>
2022-04-24 19:18:24 -0400

    remove iframes fb

--- pages/talks.vue
+++ pages/talks.vue
@@ -16,31 +16,5 @@
       <p class="text-lime-300 text-lg p-5">Innovadores al Fin del Mundo
       </p>
     </div>
-
-    <div class="w-full max-w-fit border border-violet-600 bg-slate-800 mb-10">
-      <iframe
-        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D438598426934499%26id%3D294582661336077&show_text=true&width=500"
-        width="500" height="684" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
-        allowfullscreen="true"
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-    </div>
-
-    <div class="w-full max-w-fit border border-violet-600 bg-slate-800 mb-10">
-      <iframe
-        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D536556413473098%26id%3D312620252533383&show_text=true&width=500"
-        width="500" height="684" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
-        allowfullscreen="true"
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-    </div>
-
-    <div class="w-full max-w-fit border border-violet-600 bg-slate-800 mb-10">
-      <iframe
-        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fcamaradecomer15%2Fposts%2F2136070713381901&show_text=true&width=500"
-        width="500" height="703" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
-        allowfullscreen="true"
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-    </div>
-
-
   </section>
 </template>
