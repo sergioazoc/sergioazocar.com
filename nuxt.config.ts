93d02af2229bfb56bdbacb7edb96bccd0dcdf4af
Sergio Azócar <sergioazoc@gmail.com>
2022-06-28 11:00:31 -0400

    add gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,15 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  buildModules: ['@nuxtjs/tailwindcss'],
+  buildModules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-analytics'
+  ],
   css: [
     '@fortawesome/fontawesome-svg-core/styles.css',
     '@/assets/styles/app.scss'
-  ]
+  ],
+  googleAnalytics: {
+    id: process.env.GOOGLE_ANALYTICS_ID
+  }
 })
