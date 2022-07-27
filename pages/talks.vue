8c92e9f022b39083fdb5676d90d6e94a8bee5a6e
Sergio Azócar <sergioazoc@gmail.com>
2022-07-26 22:55:32 -0400

    add talk

--- pages/talks.vue
+++ pages/talks.vue
@@ -20,6 +20,12 @@ useHead({
       en diversas instancias para compartir mi conocimiento, aquí iré recopilando algunas de ellas.</p>
 
     <div class="w-full max-w-2xl border border-violet-600 bg-slate-800 mb-10">
+      <iframe class="w-full max-w-2xl aspect-video" src="https://www.youtube.com/embed/nlEHJXhqtQM" title="The Vueniverse" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <p class="text-lime-300 text-lg p-5">The Vueniverse
+      </p>
+    </div>
+    
+    <div class="w-full max-w-2xl border border-violet-600 bg-slate-800 mb-10">
       <iframe class="w-full max-w-2xl aspect-video" src="https://www.youtube.com/embed/2-mueX2lm6s"
         title="YouTube video player" frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -27,5 +33,6 @@ useHead({
       <p class="text-lime-300 text-lg p-5">Innovadores al Fin del Mundo
       </p>
     </div>
+
   </section>
 </template>
