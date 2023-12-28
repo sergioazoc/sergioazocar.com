ef947b879730494a867173dcc74e7a3dbbf63547
Sergio Azócar <sergioazoc@gmail.com>
2023-12-27 23:46:30 -0300

    chore: upgrade dependencies

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
 
 <template>
   <section class="max-w-[600px] self-center mx-auto">
-    <h1 class="text-center text-4xl font-semibold">
+    <h1 class="text-center text-4xl font-semibold text-pretty">
       Hola! <span class="animate-hello origin-[70%_70%] inline-block">👋</span> Soy
       Sergio Azócar, un <span class="bg-clip-text text-transparent bg-gradient-to-r from-vue-green to-vue-purple font-mono">Frontend Developer</span>
       en el multiverso...
